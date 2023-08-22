3e6c96f3feafb4be0bd6d6ef0c1ee532e8d29d83
mrthnby <meralihanbay@gmail.com>
2023-08-22 21:45:00 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mert Ali Hanbay on 22.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
